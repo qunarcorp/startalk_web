2ec7ba98b8acfffb4f391735bd97fc5ccba49364
yuelin.liu <yuelin.liu@qunar.com>
2019-05-10 15:34:57 +0800

    config

--- web_config.js
+++ web_config.js
@@ -2,9 +2,9 @@
  * 根据导航连接返回的json数据，找到相应的值
  * */
 module.exports = {
-    "domain":"qtalk.test.org",
-    "xmpp":"100.80.128.247",
-    "websocket":"100.80.128.247:5280",
-    "javaurl":"http://100.80.128.247:8080/package",
+    "domain":"",
+    "xmpp":"",
+    "websocket":"",
+    "javaurl":"",
     "emails":""
 }
