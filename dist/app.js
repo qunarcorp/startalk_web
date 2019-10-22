dfa6153ec61b5decd03c8d3631806c8516f8c0a1
董超 <chaos.@qunar.com>
2019-10-22 11:54:42 +0800

    提交新dist

--- dist/app.js
+++ dist/app.js
@@ -88,7 +88,7 @@ const {
 } = process.env;
 global.startalkNavConfig = {};
 global.startalkKeys = {};
-(0, _request2.default)(`${BASEURL}/newapi/nck/qtalk_nav.qunar`, (error, response, body) => {
+(0, _request2.default)(`${BASEURL}/startalk_nav`, (error, response, body) => {
   if (!error && response.statusCode == 200) {
     global.startalkNavConfig = JSON.parse(body);
     (0, _request2.default)(`${global.startalkNavConfig.baseaddess.javaurl}/qtapi/nck/rsa/get_public_key.do`, (error, response, body) => {
